091a38cf87f10f28ff0663f541cd306f3434f9ae
Valmorbus <borgstrom.simon@gmail.com>
2016-05-03 14:51:05 +0200

    errors with put/post in rest

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,45 +1,53 @@
 
 $(document).ready(function () {
     $('#submitbtn').click(function (event) {
+        alert(toJson());
         $.ajax({
-            url: 'http://localhost:8080/SimonBorgstromIn2JavaBackend/myservlet',
+            url: 'http://localhost:12683/SimonBorgstromIn2JavaBackend/webresources/todo',
             type: 'post',
-            // dataType: 'json',
-            data: $('#form').serialize(),
+             headers: {
+            'Content-Type': 'application/json'
+        },
+            dataType: 'JSON',
+           // contentType: 'application/json',
+            data: toJson(),//$('#form').serialize(),
             success: function (data)
             {
                 console.log("test");
                 updateList();
-
             }
         });
-        event.preventDefault();
+       //event.preventDefault();
+     
     });
 });
 
 function updateList() {
     $("#table tbody").empty();
-    $.getJSON("http://localhost:8080/SimonBorgstromIn2JavaBackend/myservlet", function (data) {
+    $.getJSON("http://localhost:12683/SimonBorgstromIn2JavaBackend/webresources/todo", function (data) {
         $.each(data, function (i, fromservlet) {
-            $("#table tbody").append("<tr><td>" + fromservlet.Description +
-                    "</td><td id=\"editdate\">" + fromservlet.Duedate + "</td><td id=\"inlineCheckbox\">" +
+            var connected = '';
+            if (fromservlet.done === true){
+                connected = 'checked';
+            }
+            $("#table tbody").append("<tr><td>" + fromservlet.description +
+                    "</td><td id=\"editdate\">" + fromservlet.duedate + "</td><td id=\"inlineCheckbox\">" +
                     "<input type=\"checkbox\" id=\"inlineCheckbox1\"  \n\
-                    name=\"done\"" + fromservlet.Done + " > Klart" + "</td></tr>");
+                    name=\"done\"" + connected + " > Klart" + "</td></tr>");
         });
     });
 }
 
 $(document).ready(function () {
     $("#table").delegate('#editdate', 'click', function (e) {
-        e.preventDefault();
+      //  e.preventDefault();
         var text = prompt("Change date?", "new date");
         //  $(this).html(text);
         $.ajax({
-            url: 'http://localhost:8080/SimonBorgstromIn2JavaBackend/myservlet',
-            type: 'post',
-            // dataType: 'json',
-            data: {'key': $(this).parent().index()
-                , 'duedate': text},
+            url: 'http://localhost:12683/SimonBorgstromIn2JavaBackend/webresources/todo/'+($(this).parent().index()+1),
+            type: 'put',
+            dataType: 'json',
+            data: { 'duedate': text},
             success: function (data)
             {
                 updateList();
@@ -51,11 +59,11 @@ $(document).ready(function () {
 $(document).ready(function () {
     $("#table").delegate('#inlineCheckbox', 'click', function (e) {
         $.ajax({
-            url: 'http://localhost:8080/SimonBorgstromIn2JavaBackend/myservlet',
-            type: 'post',
-            // dataType: 'json',
-            data: {'key': $(this).parent().index()
-                , 'done': $("#inlineCheckbox").is(":checked")},
+            url: 'http://localhost:12683/SimonBorgstromIn2JavaBackend/webresources/todo/'+($(this).parent().index()+1),
+            type: 'put',
+            dataType: 'json',
+            data: {
+                 'done': $("#inlineCheckbox").is(":checked").val()},
             success: function (data)
             {
                 updateList();
@@ -68,3 +76,11 @@ $(document).ready(function () {
 $(document).ready(function () {
     updateList();
 });
+
+function toJson(){
+    return JSON.stringify({
+       "description":$('#description').val(),
+       "due date":$('#duedate').val().toString(),
+       "done":"false"
+    });
+}
